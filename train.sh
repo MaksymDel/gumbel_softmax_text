2bf8c2b091cf6e60750d66bbf799217bfd61c419
Maksym Del <maksym95@rocket.hpc.ut.ee>
2018-10-17 14:15:07 +0300

    remote changes

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-allennlp train --include-package my_library experiments/vanilla_gumbel_at_train_and_inference.json -s models/gumbel_train_and_inf_1.0_false
+allennlp train --include-package my_library experiments/vanilla_gumbel_at_train_and_inference.json -s models/gumbel_train_and_inf_0.7_true
